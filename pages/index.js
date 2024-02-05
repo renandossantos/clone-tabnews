b0985f1e960a64552b37b8203af402b409cdd3e7
Renan dos Santos <40153166+renandossantos@users.noreply.github.com>
2024-02-05 16:27:22 +0000

    modificacao no texto da pagina inicial

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Teste 2</h1>
+    return <h1>Esse é o primeiro passo que estou dando no curso de Dev! E tu é a primeira pessoa que eu quero dividir isso! Te amo, Carolinda!</h1>
 }
 
 export default Home;
