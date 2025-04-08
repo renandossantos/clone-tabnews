31db224c6738b984eab81aa859588726c643d7dd
Renan dos Santos <40153166+renandossantos@users.noreply.github.com>
2025-04-08 13:33:52 +0000

    feat: add `MethodNotAllowedError` and `ServiceError`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,14 @@
+import { createRouter } from 'next-connect'
 import database from "infra/database.js";
-import { InternalServerError } from "infra/errors";
+import controller from "infra/controller.js"
 
-async function status(request, response) {
-  try {
+const router = createRouter();
+
+router.get(getHandler);
+
+export default router.handler(controller.errorHandlers);
+
+async function getHandler(request, response) {
     const updatedAt = new Date().toISOString();
 
     const databaseVersionResult = await database.query("SHOW server_version;");
@@ -32,16 +38,4 @@ async function status(request, response) {
         },
       },
     });
-  } catch (error) {
-    const publicErrorObject = new InternalServerError({
-      cause: error,
-    });
-
-    console.log("\n Erro dentro do catch do controller:");
-    console.error(publicErrorObject);
-
-    response.status(500).json(publicErrorObject);
-  }
 }
-
-export default status;
